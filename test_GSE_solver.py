9fadcffbb5ae64728a946e15b33fb7892060e194
nielspeng <64427093+XiaonanPeng@users.noreply.github.com>
2024-09-29 22:56:21 +0800

    Update test_GSE_solver.py

--- test_GSE_solver.py
+++ test_GSE_solver.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from GSE_solver import GSE_solver
+from GSE_solver import GSE
 
 def test_GSE_solver():
     """
@@ -9,27 +9,28 @@ def test_GSE_solver():
     # set random seed
     tf.random.set_seed(1)
     # set matrix size and dtype
-    n = 5
-    m = 5
+    n = 20
+    b = 5
     dtype = tf.float32
     # generate randomly the coefficient matrices
-    A = tf.random.normal((n+m, n+m), dtype=dtype)
-    C = tf.random.normal((n+m, n+m), dtype=dtype)
-    D = tf.random.normal((n, n), dtype=dtype)
-    X_true = tf.random.normal((n+m, n), dtype=dtype)
+    A = tf.random.normal((b, n, n), dtype=dtype)
+    C = tf.random.normal((b, n, n), dtype=dtype)
+    D = tf.random.normal((b, n, n), dtype=dtype)
+    X_true = tf.random.normal((b, n, n), dtype=dtype)
 
     # compute the constant matrix E
-    E = -tf.matmul(A, X_true) - tf.matmul(tf.matmul(C,X_true), D)
+    E = -tf.matmul(A, X_true) - tf.matmul(tf.matmul(C, X_true), D)
 
     # compute the X from GSE_solver
-    X_computed = GSE_solver(A, C, D, E)
+    solver = GSE()
+    X_computed = solver.GSE_solver1(A, C, D, E)
 
     # compute error
     diff = X_computed - X_true
-    diff_norm = tf.norm(diff)
-    true_norm = tf.norm(X_true)
+    diff_norm = tf.norm(diff, axis=[1,2])
+    true_norm = tf.norm(X_true, axis=[1,2])
     relative_error = diff_norm / true_norm
-    print(f"Relative Error:{relative_error.numpy():.2e}")
+    print(f"Relative Error:{relative_error.numpy()}")
 
 if __name__ == "__main__":
     test_GSE_solver()
